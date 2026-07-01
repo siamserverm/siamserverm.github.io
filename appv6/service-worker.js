4db3229ab79ac8471825d68060b0212f4f66defe
siamserverm <siamserverm@gmail.com>
2026-07-02 01:16:38 +0700

    Delete appv6 directory

--- appv6/service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='als-v6-1-stable-20260629';
-const ASSETS=['./','./index.html','./assets/style.css','./assets/app.js','./assets/jszip.min.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy)).catch(()=>{});return r;}).catch(()=>caches.match(e.request)));});
